117848feacbdfbd22988eb768e1445cdff112b6e
tunnckoCore <mameto2011@gmail.com>
2017-08-08 05:17:54 +0300

    chore(contrib): add contributing guide, update license lines
    TAG: latest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * koa-better-serve <https://github.com/tunnckoCore/koa-better-serve>
  *
- * Copyright (c) 2017 Charlike Mike Reagent <@tunnckoCore> (https://i.am.charlike.online)
+ * Copyright (c) 2017 Charlike Mike Reagent <open.source.charlike@gmail.com> (https://i.am.charlike.online)
  * Released under the MIT license.
  */
 
